82b9695b513104118af94b40c15e965c3f9573e3
mahbub <ma01776879@gmail.com>
2025-09-25 15:28:31 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,30 @@
 import withPWAInit from "next-pwa";
 
-const withPWA = withPWAInit({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // disable PWA in dev
+const withPWA = require("next-pwa")({
+  dest: "public",      // where to generate the service worker
+  register: true,       // auto-register SW
+  skipWaiting: true,    // activate new SW immediately
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^\/_next\/static\/.*/, // Next.js static assets
+      handler: "StaleWhileRevalidate",
+      options: { cacheName: "static-resources" },
+    },
+    {
+      urlPattern: /^https:\/\/fonts.googleapis.com\/.*/,
+      handler: "StaleWhileRevalidate",
+      options: { cacheName: "google-fonts-stylesheets" },
+    },
+    {
+      urlPattern: /^https:\/\/fonts.gstatic.com\/.*/,
+      handler: "CacheFirst",
+      options: { cacheName: "google-fonts-webfonts", expiration: { maxEntries: 30, maxAgeSeconds: 60 * 60 * 24 * 365 } },
+    },
+  ],
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
-};
+});
 
-export default withPWA(nextConfig);
