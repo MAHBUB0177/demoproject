eb978e681a8e027bd94496155cff4ffcb88d5bc8
mahbub <ma01776879@gmail.com>
2025-09-25 16:11:30 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,36 @@
-import withPWAInit from "next-pwa";
+import withPWA from "next-pwa";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const runtimeCaching = [
+  {
+    urlPattern: /^\/_next\/static\/.*/, // Next.js JS/CSS chunks
+    handler: "StaleWhileRevalidate",
+    options: { cacheName: "static-resources" },
+  },
+  {
+    urlPattern: /^https:\/\/fonts.googleapis.com\/.*/,
+    handler: "StaleWhileRevalidate",
+    options: { cacheName: "google-fonts-stylesheets" },
+  },
+  {
+    urlPattern: /^https:\/\/fonts.gstatic.com\/.*/,
+    handler: "CacheFirst",
+    options: { 
+      cacheName: "google-fonts-webfonts",
+      expiration: { maxEntries: 30, maxAgeSeconds: 60 * 60 * 24 * 365 },
+    },
+  },
+];
+
+const config = {
   reactStrictMode: true,
+  experimental: { esmExternals: true }, // ensure ES modules work
 };
 
-const withPWA = withPWAInit({
+export default withPWA({
+  ...config,
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  runtimeCaching: [
-    // Cache Next.js static files (_next/static)
-    {
-      urlPattern: /^\/_next\/static\/.*/,
-      handler: "StaleWhileRevalidate",
-      options: { cacheName: "static-resources" },
-    },
-    // Cache Google Fonts CSS
-    {
-      urlPattern: /^https:\/\/fonts.googleapis.com\/.*/,
-      handler: "StaleWhileRevalidate",
-      options: { cacheName: "google-fonts-stylesheets" },
-    },
-    // Cache Google Fonts files
-    {
-      urlPattern: /^https:\/\/fonts.gstatic.com\/.*/,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "google-fonts-webfonts",
-        expiration: { maxEntries: 30, maxAgeSeconds: 60 * 60 * 24 * 365 },
-      },
-    },
-  ],
+  runtimeCaching,
 });
-
-export default withPWA(nextConfig);
